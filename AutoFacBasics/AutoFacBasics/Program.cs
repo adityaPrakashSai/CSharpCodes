92bd82fc1380eaf9cdcdf6a3b8438de8d95489a0
Aditya Prakash <aditya.prakash@optym.com>
2019-06-26 18:05:23 +0530

    autofac basics trail

---
+++ AutoFacBasics/AutoFacBasics/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace AutoFacBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<PrintService>().As<IPrintService>();
+
+            var container = builder.Build();
+            var printService = container.Resolve<IPrintService>();
+            printService.PrintSomething("HelloWorld");
+            Console.ReadKey();
+        }
+    }
+}
