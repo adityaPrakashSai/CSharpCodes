d7569379f97c5a0153c0a82311c3a016c15a9bfd
Aditya Prakash <aditya.prakash@optym.com>
2019-09-16 21:18:23 +0530

    heap solution

--- Heap/Heap/Heap/Program.cs
+++ Heap/Heap/Heap/Program.cs
@@ -6,7 +6,13 @@ namespace Heap
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var heap = new Heap();
+            heap.Insert(10);
+            heap.Insert(5);
+            heap.Insert(17);
+            heap.Insert(4);
+            heap.Insert(22);
         }
     }
 }
